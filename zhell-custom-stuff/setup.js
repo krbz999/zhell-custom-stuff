39a051a89b57de9d604657ebcff17613635d21d3
Zhell <50169243+krbz999@users.noreply.github.com>
2022-06-17 01:35:08 +0200

    Delete zhell-custom-stuff directory

--- zhell-custom-stuff/setup.js
+++
@@ -1,9 +0,0 @@
-import { MODULE_TITLE, MODULE_TITLE_SHORT } from "./scripts/const.mjs";
-import { registerSettings } from "./scripts/settings.mjs";
-import { api } from "./scripts/api.mjs";
-
-Hooks.once("init", () => {
-    console.log(`${MODULE_TITLE_SHORT} | Initializing ${MODULE_TITLE}`);
-    registerSettings();
-	api.register();
-});
