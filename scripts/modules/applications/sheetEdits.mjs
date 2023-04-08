1739ca7233952ad1a5d4f25676d43c8e23f582c4
Kenneth B <kennethzhell@gmail.com>
2023-04-08 13:32:28 +0200

    Make inspiration label a toggle

--- scripts/modules/applications/sheetEdits.mjs
+++ scripts/modules/applications/sheetEdits.mjs
@@ -36,6 +36,24 @@ export class SheetEdits {
     if (this.sheet.document.type === "character") this._createExhaustion();
     if ((this.sheet.document.type === "character") && this.settings.createMoneySpender) this._createMoneySpender();
     if (this.sheet.document.type === "character") this._createNewDay();
+    if (this.sheet.document.type === "character") this._createInspirationToggle();
+  }
+
+  /** Make 'Inspiration' a toggle. */
+  _createInspirationToggle() {
+    const insp = this.html[0].querySelector(".inspiration h4");
+    insp.classList.add("rollable");
+    insp.dataset.action = "inspiration";
+    insp.addEventListener("click", this._onClickInspiration.bind(this.sheet));
+  }
+
+  /**
+   * Toggle inspiration on or off when clicking the 'label'.
+   * @param {PointerEvent} event      The initiating click event.
+   * @returns {Actor}                 The updated actor.
+   */
+  async _onClickInspiration(event) {
+    return this.document.update({"system.attributes.inspiration": !this.document.system.attributes.inspiration});
   }
 
   /** Remove the 'alignment' input. */
