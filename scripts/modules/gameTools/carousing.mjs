61071361e0691ccf90a41388ec1abada7e458231
Kenneth B <kennethzhell@gmail.com>
2023-04-09 20:01:44 +0200

    Ignore NPCs when getting total level in carousing macro

--- scripts/modules/gameTools/carousing.mjs
+++ scripts/modules/gameTools/carousing.mjs
@@ -5,7 +5,7 @@ export async function carousing({
 } = {}) {
   const table = await ZHELL.utils.getDocument(tableName, catalogName, false);
   const players = game.actors.filter(a => a.folder?.id === folderId);
-  const combinedLevel = players.reduce((acc, a) => acc + a.system.details.level, 0);
+  const combinedLevel = players.reduce((acc, a) => acc + (a.system.details.level || 0), 0);
   const partyLevel = Math.floor(combinedLevel / players.length);
   const roll = new Roll("1d100 + @level", {level: partyLevel});
   const rollMode = CONST.DICE_ROLL_MODES.PRIVATE;
