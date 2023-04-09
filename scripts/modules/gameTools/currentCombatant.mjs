2c9187844a1dcdeefcfa0f3ca9d7a6f1235d9682
Kenneth B <kennethzhell@gmail.com>
2023-04-09 20:02:21 +0200

    Add 'safety' warnings to currentCombatant macro

--- scripts/modules/gameTools/currentCombatant.mjs
+++ scripts/modules/gameTools/currentCombatant.mjs
@@ -1,13 +1,17 @@
 export async function currentCombatant() {
+  const combat = game.combat;
+  if (!combat) return ui.notifications.warn("No current combat!");
+
+  const current = game.combat.combatant.token;
+  if (!current) return ui.notifications.warn("No current combatant!");
+
   // close all sheets.
   Object.values(ui.windows).filter(w => {
     return w.document?.documentName.includes("Actor");
   }).forEach(w => w.minimize());
 
-  const current = game.combat.combatant.token;
-  const sheet = current.actor.sheet;
-
   // render current combatant.
+  const sheet = current.actor.sheet;
   if (sheet.rendered) sheet.maximize();
   else sheet.render(true, {top: 0, left: 0});
 
