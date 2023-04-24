73ab7e48df01c9e9e867072c2ec9889eec000fa5
Kenneth B <kennethzhell@gmail.com>
2023-04-24 21:55:34 +0200

    Do not minimize current combatant needlessly

--- scripts/modules/gameTools/currentCombatant.mjs
+++ scripts/modules/gameTools/currentCombatant.mjs
@@ -7,7 +7,7 @@ export async function currentCombatant() {
 
   // close all sheets.
   Object.values(ui.windows).filter(w => {
-    return w.document?.documentName.includes("Actor");
+    return w.document?.documentName.includes("Actor") && (current.actor !== w.document);
   }).forEach(w => w.minimize());
 
   // render current combatant.
