d13a7c290d664880cc9918c9c9b2b3e24ffe8362
Zhell <zhellqol@gmail.com>
2023-07-22 09:13:05 +0200

    improve time advance macro

--- scripts/modules/gameTools/advanceTime.mjs
+++ scripts/modules/gameTools/advanceTime.mjs
@@ -2,22 +2,24 @@ export async function advanceTime(s = null) {
   const content = `
   <p style="text-align: center;">Advance time by a number of seconds.</p>
   <form class="dnd5e">
-  <div class="form-group">
-    <label>Seconds:</label>
-    <div class="form-fields">
-      <input type="number" autofocus ${s ? `value="${s}"` : ""}>
+    <div class="form-group">
+      <label>Seconds:</label>
+      <div class="form-fields">
+        <input type="text" autofocus ${s ? `value="${s}"` : ""}>
+      </div>
     </div>
-  </div>
   </form>`;
   const time = await Dialog.prompt({
     title: "Advance Time",
     content,
     label: "Advance!",
     rejectClose: false,
-    callback: async (html) => html[0].querySelector("input").valueAsNumber
+    callback: async (html) => html[0].querySelector("input").value
   });
-  if (!time) return;
+  if (!time || !Roll.validate(time)) return;
+  const data = canvas.tokens.controlled[0]?.actor?.getRollData() ?? {};
+  const {total} = await new Roll(time, data).evaluate();
 
-  ui.notifications.info(`Advanced time by ${time} seconds.`);
-  return game.time.advance(time);
+  ui.notifications.info(`Advanced time by ${total} seconds.`);
+  return game.time.advance(total);
 }
