6a53fec00a1fa8d638afc6eeacead5e4a64ae8c5
Zhell <zhellqol@gmail.com>
2023-11-04 23:19:21 +0100

    Update manifest: 11.2.3

---
+++ test.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+/**
+ * Update manifest file with new version.
+ * @param {string} [v="patch"]      The number to up by 1 in the version number, either 'major', 'minor', or 'patch'.
+ */
+function updateManifest(v = "patch") {
+  const file = fs.readFileSync("module.json");
+  const obj = JSON.parse(file);
+  const old = obj.version;
+  let [major, minor, patch] = old.split(".");
+  switch (v) {
+    case "patch": patch = Number(patch) + 1; break;
+    case "minor": minor = Number(minor) + 1; patch = 0; break;
+    case "major": major = Number(major) + 1; minor = patch = 0; break;
+  }
+  const newV = [major, minor, patch].join(".");
+  obj.version = newV;
+  obj.download = `${obj.url}/releases/download/v${newV}/module.zip`;
+
+  fs.writeFileSync("module.json", JSON.stringify(obj, null, 2) + '\n');
+}
+
+module.exports = {updateManifest};
