b8c23b64852a3c0b5869373c371631ecf80b7bc6
Zhell <zhellqol@gmail.com>
2025-04-30 14:19:20 +0200

    Update data entry mangling.

--- scripts/hooks/data-entry.mjs
+++ scripts/hooks/data-entry.mjs
@@ -1,18 +1,61 @@
 /**
  * Add css classes to item or activity sheets.
- * @param {Application|ApplicationV2} sheet   The item or activity sheet.
- * @param {HTMLElement|jQuery} html           The sheet element.
+ * @param {Application} sheet   The item or activity sheet.
+ * @param {HTMLElement} html    The sheet element.
  */
 export default function dataEntry(sheet, html) {
   if (!ZHELL.settings.dataEntry) return;
   const classes = ["zhell-data-entry"];
 
-  if (sheet.activity) {
-    classes.push("activity");
-  } else {
-    classes.push(sheet.document.type);
-    html = html[0];
+  const isActivity = !!sheet.activity;
+
+  if (isActivity) classes.push("activity");
+  else classes.push(sheet.document.type);
+
+  html.closest(".application").classList.add(...classes);
+
+  if (!isActivity) {
+    for (const d of ["effects", "advancement", "activities"]) {
+      const tab = html.querySelector(`[data-application-part="${d}"]`);
+      if (!tab) continue;
+
+      const button = foundry.utils.parseHTML(`
+      <button type="button" aria-label="Create" class="fake create-child gold-button always-interactive">
+        <i class="fas fa-plus" inert></i>
+      </button>`);
+
+      button.addEventListener("click", () => createChild.call(sheet, d));
+      tab.insertAdjacentElement("afterbegin", button);
+      tab.classList.add(`tab-${d}`);
+    }
+  }
+}
+
+/**
+ * Create a child document.
+ * @this {Application}
+ * @param {string} tab    Type of child.
+ * @returns {Promise}
+ */
+function createChild(tab) {
+  if (tab === "activities") {
+    return dnd5e.documents.activity.UtilityActivity.createDialog({}, {
+      parent: this.item,
+      types: Object.entries(CONFIG.DND5E.activityTypes).filter(([, { configurable }]) => {
+        return configurable !== false;
+      }).map(([k]) => k),
+    });
   }
 
-  html.closest(".app, .application").classList.add(...classes);
+  if (tab === "advancement") {
+    return dnd5e.documents.advancement.Advancement.createDialog({}, { parent: this.item });
+  }
+
+  if (tab === "effects") {
+    return ActiveEffect.implementation.create({
+      name: this.document.name,
+      img: this.document.img,
+      origin: this.document.uuid,
+    }, { parent: this.document, renderSheet: true });
+  }
 }
