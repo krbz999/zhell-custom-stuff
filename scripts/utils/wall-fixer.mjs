4d4fa92e6447e3b8aed359c92d3f0976a828cead
Zhell <zhellqol@gmail.com>
2025-05-04 01:47:35 +0200

    Update wallfixer macro.

--- scripts/utils/wall-fixer.mjs
+++ scripts/utils/wall-fixer.mjs
@@ -1,17 +1,23 @@
-export default async function wallFixer() {
-  let updates;
-  if (!event.shiftKey) {
-    const { BOTH, LEFT, RIGHT } = CONST.WALL_DIRECTIONS;
-    const obj = { [BOTH]: LEFT, [LEFT]: RIGHT, [RIGHT]: BOTH };
-    updates = canvas.walls.controlled.map(i => {
-      return { _id: i.id, dir: obj[i.document.dir] };
-    });
-  } else {
-    const { NONE, NORMAL } = CONST.WALL_MOVEMENT_TYPES;
-    updates = canvas.walls.controlled.map(i => {
-      const move = i.document.move === NONE ? NORMAL : NONE;
-      return { _id: i.id, move };
-    });
+export default async function wallFixer({ cycleDirection = false, cycleMovementType = true } = {}) {
+  const { BOTH, LEFT, RIGHT } = CONST.WALL_DIRECTIONS;
+  const { NONE, NORMAL } = CONST.WALL_MOVEMENT_TYPES;
+
+  const updates = [];
+
+  for (const wall of canvas.walls.controlled) {
+    const update = { _id: wall.id };
+    if (cycleDirection) {
+      update.dir = (wall.document.dir === BOTH)
+        ? LEFT
+        : (wall.document.dir === LEFT)
+          ? RIGHT
+          : BOTH;
+    }
+    if (cycleMovementType) {
+      update.move = (wall.document.move === NONE) ? NORMAL : NONE;
+    }
+    updates.push(update);
   }
+
   return canvas.scene.updateEmbeddedDocuments("Wall", updates);
 }
