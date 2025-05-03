3193df40abacf61ecf7f6eba141a45c5564e1c1f
Zhell <zhellqol@gmail.com>
2025-05-04 01:32:35 +0200

    Fix Reaction icon. Fix i18n sort.

--- scripts/hooks/system-changes.mjs
+++ scripts/hooks/system-changes.mjs
@@ -17,7 +17,7 @@ export default function systemChanges() {
   CONFIG.DND5E.conditionTypes.reaction = {
     id: "reaction",
     name: "ZHELL.CONDITION.REACTION.NAME",
-    img: "assets/images/conditions/reaction.webp",
+    img: "systems/dnd5e/icons/svg/activity/forward.svg",
     duration: { rounds: 1 },
     description: "<p>You have spent your reaction. You cannot take another reaction until the start of your next turn.</p>",
   };
